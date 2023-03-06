ce69a08d5361ba40de83b8297652dac6867e72be
Bahareh Amricachi <baharehamri@gmail.com>
2023-03-06 14:37:47 -0800

    Exercise 1 completed

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert Number 1");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insert Operator");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Insert Number 2");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            if (op == "+")
+                result = num1 + num2;
+            else if (op == "-")
+                result = num1 - num2;
+            else if (op == "*")
+                result = num1 * num2;
+            else if (op == "/")
+                result = num1 / num2;
+            Console.WriteLine(result);
         }
     }
 }
